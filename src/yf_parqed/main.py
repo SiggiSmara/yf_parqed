2de536c0756f7f96a108e15cbd9e0471611cb892
Sigurður Smárason <8719966+SiggiSmara@users.noreply.github.com>
2024-12-21 08:55:00 +0100

    move to a class structure

--- src/yf_parqed/main.py
+++ src/yf_parqed/main.py
@@ -3,47 +3,72 @@ import httpx
 from pathlib import Path
 import os
 
-from yf_parqed.get_data_parquet import (
-    get_new_list_of_stocks,
-    save_current_list_of_stocks,
-    save_not_founds,
-)
-
+from yf_parqed.primary_class import YFParqed
 
 app = typer.Typer()
 
 global wrk_dir
 wrk_dir = Path(os.getcwd())
+yf_parqed = YFParqed(my_path=wrk_dir)
 
 
 def download_file(url: str, local_path: Path):
     res = httpx.get(url, follow_redirects=True)
-    print(res)
     local_path.write_text(res.text)
-
-
-@app.callback()
-def set_wd(
-    wrk_dir: str = typer.Option(
-        os.getcwd(), help="Working directory, default is current directory"
-    ),
-):
-    wrk_dir = Path(wrk_dir)
 
 
 def get_tickers():
     url = "https://datahub.io/core/nasdaq-listings/_r/-/data/nasdaq-listed.csv"
-    local_path = wrk_dir / "nasdaq-listed.csv"
+    local_path = yf_parqed.my_path / "nasdaq-listed.csv"
     download_file(url, local_path)
 
     url = "https://datahub.io/core/nyse-other-listings/_r/-/data/nyse-listed.csv"
-    local_path = wrk_dir / "nyse-listed.csv"
+    local_path = yf_parqed.my_path / "nyse-listed.csv"
     download_file(url, local_path)
+
+
+def define_intervals():
+    global yf_parqed
+    intervals = [
+        "1d",
+        "1h",
+    ]
+    yf_parqed.save_intervals(intervals)
+
+
+@app.callback()
+def main(
+    wrk_dir: str = typer.Option(
+        os.getcwd(), help="Working directory, default is current directory"
+    ),
+    limits: tuple[int, int] = typer.Option(
+        (2, 5),
+        help="API Rate limiting. First argument is the maximum number of requests allowed in the time duration. Second argument is the time duration in seconds.",
+    ),
+):
+    global yf_parqed
+    wrk_dir = Path(wrk_dir)
+    yf_parqed.set_working_path(wrk_dir)
+    yf_parqed.set_limiter(max_requests=limits[0], duration=limits[1])
 
 
 @app.command()
 def initialize():
+    global yf_parqed
     get_tickers()
-    tickers = get_new_list_of_stocks(my_path=wrk_dir)
-    save_current_list_of_stocks(tickers, my_path=wrk_dir)
-    save_not_founds([], my_path=wrk_dir)
+    define_intervals()
+    tickers = yf_parqed.get_new_list_of_stocks()
+    yf_parqed.save_current_list_of_stocks(tickers)
+    yf_parqed.save_not_founds([])
+
+
+@app.command()
+def add_interval(interval: str):
+    global yf_parqed
+    yf_parqed.add_interval(interval)
+
+
+@app.command()
+def remove_interval(interval: str):
+    global yf_parqed
+    yf_parqed.remove_interval(interval)
---
+++ src/yf_parqed/primary_class.py
@@ -0,0 +1,297 @@
+import yfinance as yf
+import pandas as pd
+from pathlib import Path
+from datetime import datetime, timedelta
+import json
+from tqdm import tqdm
+from loguru import logger
+
+from requests import Session
+from requests_ratelimiter import LimiterMixin, MemoryQueueBucket
+from pyrate_limiter import Duration, RequestRate, Limiter
+
+
+class LimiterSession(LimiterMixin, Session):
+    pass
+
+
+class YFParqed:
+    def __init__(
+        self,
+        my_path: Path = None,
+        my_intervals: list = ["1d", "1h"],
+        ny_session: Session = None,
+    ):
+        self.my_path = None
+        self.set_working_path(my_path)
+        self.my_intervals = my_intervals
+        self.new_not_found = False
+        if ny_session is None:
+            self.ny_session = LimiterSession(
+                limiter=Limiter(
+                    RequestRate(2, Duration.SECOND * 5)
+                ),  # max 2 requests per 5 seconds
+                bucket_class=MemoryQueueBucket,
+            )
+        else:
+            self.ny_session = ny_session
+
+    def set_working_path(self, my_path: Path):
+        if my_path is None:
+            self.my_path = Path.cwd()
+        else:
+            self.my_path = my_path
+        self.update_meta_after_path_change()
+        return self.my_path
+
+    def update_meta_after_path_change(self):
+        self.current_stocks_path = self.my_path / "current_tickers.json"
+        self.not_founds_path = self.my_path / "not_found_tickers.json"
+        self.intervals_path = self.my_path / "intervals.json"
+        self.load_current_not_founds()
+        self.load_current_list_of_stocks()
+
+    def set_limiter(self, max_requests: int = 2, duration: int = 5):
+        self.session = LimiterSession(
+            limiter=Limiter(
+                RequestRate(max_requests, Duration.SECOND * duration)
+            ),  # max 2 requests per 5 seconds
+            bucket_class=MemoryQueueBucket,
+        )
+
+    def load_intervals(self):
+        if self.intervals_path.is_file():
+            self.my_intervals = json.loads(self.intervals_path.read_text())
+        else:
+            self.my_intervals = []
+
+    def save_intervals(self, intervals: list):
+        self.intervals_path.write_text(json.dumps(intervals, indent=4))
+        self.my_intervals = intervals
+
+    def add_interval(self, interval: str):
+        self.my_intervals.append(interval)
+        self.save_intervals(self.my_intervals)
+
+    def remove_interval(self, interval: str):
+        self.my_intervals = [x for x in self.my_intervals if x != interval]
+        self.save_intervals(self.my_intervals)
+
+    def get_new_list_of_stocks(self):
+        nasdaq_path = self.my_path / "nasdaq-listed.csv"
+        nyse_path = self.my_path / "nyse-listed.csv"
+        if not nasdaq_path.is_file() or not nyse_path.is_file():
+            logger.debug("Nasdaq and/or Nyse file not found.  Nothing to do")
+            return []
+
+        nasdaq = [
+            x
+            for x in [
+                y.split(",")[0]
+                for y in nasdaq_path.read_text().split("\n")
+                if len(y.strip()) > 0
+            ]
+            if x is not None or x != "" or not x.startswith("File Creation Time")
+        ]
+
+        nyse = [
+            x
+            for x in [
+                y.split(",")[0]
+                for y in nyse_path.read_text().split("\n")
+                if len(y.strip()) > 0
+            ]
+            if x is not None or x != ""
+        ]
+        stocks = nasdaq[1:] + nyse[1:]
+        stocks = [
+            {
+                "ticker": x,
+                "added_date": datetime.now().strftime("%Y-%m-%d"),
+            }
+            for x in stocks
+        ]
+        return stocks
+
+    def load_current_list_of_stocks(self):
+        if self.current_stocks_path.is_file():
+            self.stocks = json.loads(self.current_stocks_path.read_text())
+        else:
+            self.stocks = []
+
+    def save_current_list_of_stocks(self, stocks: list):
+        stocks = [x for x in stocks if x["ticker"] not in self.not_founds]
+        self.current_stocks_path.write_text(json.dumps(stocks, indent=4))
+
+    def update_current_list_of_stocks(self, new_tickers: list):
+        curr_stocks = self.get_current_list_of_stocks()
+        curr_tickers = [x["ticker"] for x in curr_stocks]
+        for ticker in new_tickers:
+            if ticker not in curr_tickers:
+                curr_stocks.append(
+                    {
+                        "ticker": ticker,
+                        "added_date": datetime.now().strftime("%Y-%m-%d"),
+                    }
+                )
+        self.save_current_list_of_stocks(stocks=curr_stocks)
+
+    def load_current_not_founds(self):
+        if self.not_founds_path.is_file():
+            self.not_founds_whole = json.loads(self.not_founds_path.read_text())
+            self.not_founds = [x["ticker"] for x in self.not_founds_whole]
+        else:
+            self.not_founds_whole = []
+            self.not_founds = []
+
+    def save_not_founds(self):
+        self.not_founds_path.write_text(json.dumps(self.not_founds_whole, indent=4))
+
+    def save_yf(self, df1, df2, data_path):
+        if df2.empty:
+            df2 = df1
+        elif df1.empty:
+            logger.debug("d1 empty.. nothing to do")
+            return df2
+        df1.reset_index(inplace=True)
+        df2.reset_index(inplace=True)
+        df2 = pd.concat([df2, df1], axis=0).drop_duplicates(
+            subset=["date", "stock"], keep="last"
+        )
+        df2.to_parquet(data_path, index=False, compression="gzip")
+        return df2.set_index(["stock", "date"])
+
+    def read_yf(self, data_path: Path):
+        empty_df = pd.DataFrame(
+            columns=[
+                "stock",
+                "date",
+                "open",
+                "high",
+                "low",
+                "close",
+                "volume",
+                "sequence",
+            ]
+        ).set_index(["stock", "date"])
+        if data_path.is_file():
+            df = pd.read_parquet(data_path)
+            if df.empty:
+                logger.debug(
+                    f"Empty dataframe found for {data_path.stem}, deleting it before looking for new data"
+                )
+                data_path.unlink()
+                return empty_df
+            df.set_index(["stock", "date"], inplace=True)
+            return df
+        else:
+            return empty_df
+
+    def update_stock_data(
+        self, start_date: datetime, end_date: datetime, update_only: bool = True
+    ):
+        self.new_not_found = False
+        for stock in tqdm(self.stocks, desc="Processing stocks"):
+            if stock["ticker"] in self.not_found:
+                continue
+            for interval in self.my_intervals:
+                self.save_single_stock_data(
+                    stock=stock["ticker"],
+                    start_date=start_date,
+                    end_date=end_date,
+                    interval=interval,
+                    update_only=update_only,
+                )
+
+    def save_single_stock_data(
+        self,
+        stock: str,
+        start_date: datetime,
+        end_date: datetime,
+        interval: str = "1d",
+        update_only: bool = True,
+        not_found: list = None,
+    ):
+        logger.debug(stock)
+        data_path = self.my_path / f"stocks_{interval}" / f"{stock}.parquet"
+
+        if stock in self.not_found:
+            return
+        data_path.parent.mkdir(parents=True, exist_ok=True)
+        df2 = self.read_yf(data_path)
+        if not df2.empty and update_only:
+            start_date = df2.index.get_level_values("date").max().to_pydatetime()
+            logger.debug(f"Updating {stock} from {start_date} to {end_date}")
+
+        if (end_date - start_date).days > 0:
+            df1 = self.get_yfinance_data(
+                stock=stock, start_date=start_date, end_date=end_date, interval=interval
+            )
+            # print(df1)
+            # print(df1.shape[0])
+            if df1.shape[0] > 0:
+                self.save_yf(df1, df2, data_path)
+            else:
+                logger.debug(
+                    f"{stock} returned no results for the date range of {start_date} to {end_date}.  Putting it on the not found list."
+                )
+                self.not_founds_whole.append(
+                    {
+                        "ticker": stock,
+                        "start_date": datetime.now().strftime("%Y-%m-%d"),
+                    }
+                )
+                self.not_founds.append(stock)
+                self.new_not_found = True
+
+        return df2
+
+    def process_yfinance_data(self, df: pd.DataFrame, ticker: str) -> pd.DataFrame:
+        df = df.rename_axis("date").reset_index()
+        df["date"] = df["date"].dt.tz_localize(None)
+        df.columns = [col.lower() for col in df.columns]
+        df["stock"] = ticker
+        return df[
+            ["date", "open", "high", "low", "close", "volume", "stock"]
+        ].set_index(["stock", "date"])
+
+    def get_yfinance_data(
+        self, stock: str, start_date: datetime, end_date: datetime, interval: str = "1d"
+    ) -> pd.DataFrame:
+        ticker = yf.Ticker(stock, session=self.session)
+        if interval in ("60m", "90m", "1h"):
+            # make sure the day limit is not reached
+            today = datetime.now()
+            if (today - start_date).days >= 730:
+                start_date = today - timedelta(729)
+
+            if (today - end_date).days >= 730:
+                end_date = today
+
+        if interval in ("1m", "2m", "5m", "15m", "30m"):
+            # make sure the 6 day limit is not reached
+            today = datetime.now()
+            if (today - start_date).days >= 6:
+                start_date = today - timedelta(6)
+
+            if (today - end_date).days >= 6:
+                end_date = today
+
+        df = ticker.history(start=start_date, end=end_date, interval=interval)
+        if df.empty:
+            logger.debug(
+                f"{stock} returned no results for the date range of {start_date} to {end_date}."
+            )
+            return pd.DataFrame(
+                columns=[
+                    "stock",
+                    "date",
+                    "open",
+                    "high",
+                    "low",
+                    "close",
+                    "volume",
+                    "sequence",
+                ]
+            ).set_index(["stock", "date"])
+        return self.process_yfinance_data(df, stock)
